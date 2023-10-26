71f39996b4ccff57daeb2ab747bb092bd4b6490e
Mathias Kurz <mdkurz@gmail.com>
2023-10-26 18:35:16 +0200

    fix: reverted all style split

--- pages/boutique/index.js
+++ pages/boutique/index.js
@@ -7,7 +7,19 @@ import CartSummary from '../../components/boutique/CartSummary'
 import { productsData } from '../../infrastructure/adapters/product/productsData';
 import { ProductGatewayStub } from '../../infrastructure/adapters/product/product.stub';
 
-import { useStyles } from './styles'
+const useStyles = theme => ({
+    root: {marginBottom: theme.spacing(3)},
+    h1: {
+        margin: theme.spacing(5, 0)
+    },
+    filterTitle: {
+        backgroundColor: theme.palette.primary,
+        color: theme.palette.primary.main
+    },
+    filterListItem: {
+        paddingLeft: 0,
+    }
+});
 
 export async function getStaticProps() {
    const productGateway = new ProductGatewayStub(productsData);
--- pages/favourites/index.js
+++ pages/favourites/index.js
@@ -5,7 +5,19 @@ import DefaultLayaout from '../../components/DefaultLayout'
 import ProductsList from '../../components/boutique/ProductsList'
 import useGlobalContext from '../../hooks/useGlobalContext';
 
-import { useStyles } from './styles'
+const useStyles = theme => ({
+    root: {marginBottom: theme.spacing(3)},
+    h1: {
+        margin: theme.spacing(5, 0)
+    },
+    filterTitle: {
+        backgroundColor: theme.palette.primary,
+        color: theme.palette.primary.main
+    },
+    filterListItem: {
+        paddingLeft: 0,
+    }
+});
 
 const Boutique = props => {
   const {classes} = props
