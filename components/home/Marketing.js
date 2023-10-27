279c33d58ce95b60e364d4060bb75c06196041d0
Mathias Kurz <mdkurz@gmail.com>
2023-10-27 14:45:37 +0200

    chore: improve marketing module responsiveness

--- components/home/Marketing.js
+++ components/home/Marketing.js
@@ -7,12 +7,12 @@ import marketingIcon from '../../public/static/images/icon.svg';
 
 const useStyles = theme => ({
     grid: {
-        borderRadius: '10px 20px 20px 10px',
+        borderRadius: '10px 10px 20px 20px',
         border: '1px solid #ECEFF2',
         overflow: 'hidden',
         marginBottom: theme.spacing(3),
-        [theme.breakpoints.up('sm')]: {
-            borderRadius: '10px 10px 20px 20px',
+        [theme.breakpoints.up('md')]: {
+            borderRadius: '10px 20px 20px 10px',
         },
     },
     marketingList: {
@@ -29,8 +29,11 @@ const useStyles = theme => ({
     },
     marketingImage: {
         width: '100%',
-        height: '100%',
+        height: 'clamp(150px, 20vh, 400px)',
         objectFit: 'cover',
+        [theme.breakpoints.up('md')]: {
+            height: '100%',
+        },
     },
     marketingIcon: {
         flexShrink: 0,
@@ -41,16 +44,16 @@ const Marketing = (props) => {
     const { classes, content } = props
     return (
         <Grid container className={classes.grid}>
-        <Grid item xs={12} lg={6}>
+        <Grid item xs={12} md={5} lg={6}>
             <img src={marketingImage.src} className={classes.marketingImage} />
         </Grid>
-        <Grid item xs={12} lg={6} className={classes.marketingList}>
+        <Grid item xs={12} md={7} lg={6} className={classes.marketingList}>
             {content?.map((item) => (
                 <Box key={item.id} className={classes.marketingItem}>
                     <Box className={classes.marketingIcon}>
-                        <Image src={marketingIcon} width={57} layout='fixed'/>
+                        <Image src={marketingIcon}/>
                     </Box>
-                    <Box sx={{ flexGrow: 0}}>
+                    <Box>
                         <Typography variant="h6" gutterBottom>
                             {item.title}
                         </Typography>
